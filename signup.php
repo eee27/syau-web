97249be0d60ee0b665c997f5070e445657239536
liu bo <916321714@qq.com>
2015-05-07 22:02:15 +0800

    lb/5/7-2
    登录注册界面基本完成  主页框架完成 准备着手js和后台

---
+++ signup.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+		<h1>your name is
+			<?php
+				echo $_POST["username"];
+
+			?>
+		</h1>
+		<h1>your email is
+			<?php
+				echo $_POST["email"];
+
+			?>
+		</h1>
+
+	</body>	
+</html>
